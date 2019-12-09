f2d8482507d853161a12d247ce1db681b93017c4
Dustin Farris <dustin@Dustins-MacBook-Pro.local>
2019-12-08 16:09:29 -0800

    update setup scripts

--- setup.sh
+++ setup.sh
@@ -44,41 +44,9 @@ brew bundle
 cd -
 
 #==============
-# Install vim-plug for neovim
-#==============
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-#==============
 # Set fish as the default shell
 #==============
 chsh -s /usr/local/bin/fish
 
-#==============
-# Set up python for neovim
-#==============
-# Install pyenv
-curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-exec $SHELL
-# When updating the python version, the path in config/nvim/init.vim will need
-# to be updated as well
-# == Python 2
-pyenv install 2.7.17
-pyenv virtualenv 2.7.17 neovim2
-pyenv activate neovim2
-pip install neovim
-# == Python 3
-pyenv install 3.8.0
-pyenv virtualenv 3.8.0 neovim3
-pyenv activate neovim3
-pip install neovim
-pip install flake8
-ln -s (pyenv which flake8) /usr/local/bin/flake8
-
-
-#==============
-# And we are done
-#==============
-echo -e "\n====== All Done!! ======\n"
-echo
-echo "Don't forget to run :UpdateRemotePlugins in neovim"
+echo "Restart the shell (fish)"
+echo "then run setup.fish"
