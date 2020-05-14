88fdde6ad08b7fed8b586f9de2189fe9efa0d6ed
Dustin Farris <dustin@farris.io>
2020-05-14 09:54:24 -0700

    set aws region in dlp function

--- config/fish/functions/dlp.fish
+++ config/fish/functions/dlp.fish
@@ -2,6 +2,7 @@ function dlp
     set stack $argv[1]
     read -sP "Enter the pulumi stack passphrase for "$stack": " passphrase
     set -x PULUMI_CONFIG_PASSPHRASE $passphrase
+    set -x AWS_REGION us-west-2
     pulumi stack select UCB66D870DA/$stack
     aws-shib exec dladmin-ucboitlake$stack --
 end
